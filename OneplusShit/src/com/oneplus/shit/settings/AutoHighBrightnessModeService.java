SuperDroidBond/device_oneplus_OneplusShit

OneplusShit: Allow to set Automatic High brightness mode threshold

OneplusShit/src/com/oneplus/shit/settings/AutoHighBrightnessModeService.java
@@ -6,14 +6,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.preference.PreferenceManager;
 
 import com.oneplus.shit.settings.R;
+import com.oneplus.shit.settings.ShitPanelSettings;
 import com.oneplus.shit.settings.utils.FileUtils;
 
 public class AutoHighBrightnessModeService extends Service {
@@ -23,6 +26,8 @@ public class AutoHighBrightnessModeService extends Service {
 
     private SensorManager mSensorManager;
     Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
 
     public void activateLightSensorRead() {
         mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
@@ -55,13 +60,14 @@ public class AutoHighBrightnessModeService extends Service {
             KeyguardManager km =
                     (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
             boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
-            if (lux > 6500.0f) {
+            float threshold = Float.parseFloat(mSharedPrefs.getString(ShitPanelSettings.KEY_HBM_AUTOBRIGHTNESS_THRESHOLD, "30000"));
+            if (lux > threshold) {
                 if ((!mAutoHBMActive | !isCurrentlyEnabled()) && !keyguardShowing) {
                     mAutoHBMActive = true;
                     enableHBM(true);
                 }
             }
-            if (lux < 6500.0f) {
+            if (lux < threshold) {
                 if (mAutoHBMActive) {
                     mAutoHBMActive = false;
                     enableHBM(false);
@@ -91,6 +97,7 @@ public class AutoHighBrightnessModeService extends Service {
         IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         if (pm.isInteractive()) {
             activateLightSensorRead();

OneplusShit/src/com/oneplus/shit/settings/HBMAutobrightnessThresholdPreference.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.oneplus.shit.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+public class HBMAutobrightnessThresholdPreference extends ProperSeekBarPreference {
+
+    private static int mMinVal = 0;
+    private static int mMaxVal = 60000;
+    private static int mDefVal = 30000;
+
+    public HBMAutobrightnessThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1000;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceSettings.KEY_HBM_AUTOBRIGHTNESS_THRESHOLD, "30000"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceSettings.KEY_HBM_AUTOBRIGHTNESS_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}
+

ColtOS-Devices/android_device_oneplus_OneplusShit

OneplusShit/src/com/oneplus/shit/settings/AutoHighBrightnessModeService.java
@@ -6,14 +6,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
 
 import com.oneplus.shit.settings.R;
+import com.oneplus.shit.settings.ShitPanelSettings;
 import com.oneplus.shit.settings.utils.FileUtils;
 
 public class AutoHighBrightnessModeService extends Service {
@@ -23,6 +26,8 @@ public class AutoHighBrightnessModeService extends Service {
 
     private SensorManager mSensorManager;
     Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
 
     public void activateLightSensorRead() {
         mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
@@ -55,13 +60,14 @@ public class AutoHighBrightnessModeService extends Service {
             KeyguardManager km =
                     (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
             boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
-            if (lux > 6500.0f) {
+            float threshold = Float.parseFloat(mSharedPrefs.getString(ShitPanelSettings.KEY_HBM_AUTOBRIGHTNESS_THRESHOLD, "30000"));
+            if (lux > threshold) {
                 if ((!mAutoHBMActive | !isCurrentlyEnabled()) && !keyguardShowing) {
                     mAutoHBMActive = true;
                     enableHBM(true);
                 }
             }
-            if (lux < 6500.0f) {
+            if (lux < threshold) {
                 if (mAutoHBMActive) {
                     mAutoHBMActive = false;
                     enableHBM(false);
@@ -91,6 +97,7 @@ public class AutoHighBrightnessModeService extends Service {
         IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         if (pm.isInteractive()) {
             activateLightSensorRead();

OneplusShit/src/com/oneplus/shit/settings/HBMAutobrightnessThresholdPreference.java
@@ -1,0 +1,61 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.oneplus.shit.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+import com.oneplus.shit.settings.R;
+import com.oneplus.shit.settings.ShitPanelSettings;
+
+public class HBMAutobrightnessThresholdPreference extends ProperSeekBarPreference {
+
+    private static int mMinVal = 0;
+    private static int mMaxVal = 60000;
+    private static int mDefVal = 30000;
+
+    public HBMAutobrightnessThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1000;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(ShitPanelSettings.KEY_HBM_AUTOBRIGHTNESS_THRESHOLD, "30000"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(ShitPanelSettings.KEY_HBM_AUTOBRIGHTNESS_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}
+